e2b1898bd9636d04e30b432cc3304c457f397643
Tint <98thintzawhtun@gmail.com>
2023-06-07 02:48:41 +0630

    second commit

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,13 @@
 import { Box, Flex } from '@mantine/core';
 import React from 'react'
 import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
 
 const Layout = ({ children }) => {
     return (
       <Flex className=''>
         <Sidebar />
+        <Navbar/>
         <Box p={20}>{children}</Box>
       </Flex>
     );
